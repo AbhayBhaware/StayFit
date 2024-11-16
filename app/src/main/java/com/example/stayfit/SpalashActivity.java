dab08ecaade7cb6b3bc0794b5649c19282a1c724
AbhayBhaware <abhaybhaware20@gmail.com>
2024-11-16 22:06:06 +0530

    Login Frontend and Backend

--- app/src/main/java/com/example/stayfit/SpalashActivity.java
+++ app/src/main/java/com/example/stayfit/SpalashActivity.java
@@ -2,7 +2,9 @@ package com.example.stayfit;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class SpalashActivity extends AppCompatActivity {
 
@@ -10,5 +12,15 @@ public class SpalashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_spalash);
+
+        Handler h=new Handler();
+        h.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i=new Intent(SpalashActivity.this,LoginActivity.class);
+                startActivity(i);
+                finish();
+            }
+        },4000);
     }
 }
