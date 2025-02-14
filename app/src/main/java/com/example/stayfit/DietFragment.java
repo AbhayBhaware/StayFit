08d47bc59adfee30f77b1dcfb2552f71119b69fa
AbhayBhaware <abhaybhaware20@gmail.com>
2025-02-14 20:56:27 +0530

    All Diet Fragment Added

--- app/src/main/java/com/example/stayfit/DietFragment.java
+++ app/src/main/java/com/example/stayfit/DietFragment.java
@@ -3,62 +3,91 @@ package com.example.stayfit;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.Toast;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link DietFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+
 public class DietFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public DietFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment DietFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static DietFragment newInstance(String param1, String param2) {
-        DietFragment fragment = new DietFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
+    ImageView i1_weightloss, i2_musclegain, i3_balanced, i4_highprotein;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_diet, container, false);
+        View v= inflater.inflate(R.layout.fragment_diet, container, false);
+
+        i1_weightloss=v.findViewById(R.id.weightlossimg);
+        i2_musclegain=v.findViewById(R.id.musclegainImg);
+        i3_balanced=v.findViewById(R.id.balanceddietimg);
+        i4_highprotein=v.findViewById(R.id.highprotienimg);
+
+        i1_weightloss.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Fragment weightlossFrag=new WeightLossFragment();
+                FragmentManager fragmentManager=getParentFragmentManager();
+                FragmentTransaction transaction=fragmentManager.beginTransaction();
+                transaction.replace(R.id.framelayout,weightlossFrag);
+                transaction.addToBackStack(null);
+                transaction.commit();
+
+                Toast.makeText(getContext(), "Weight Loss Diet", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        i2_musclegain.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Fragment musclegainFrag=new MuscleGainFragment();
+                FragmentManager fragmentManager=getParentFragmentManager();
+                FragmentTransaction transaction=fragmentManager.beginTransaction();
+                transaction.replace(R.id.framelayout,musclegainFrag);
+                transaction.addToBackStack(null);
+                transaction.commit();
+
+                Toast.makeText(getContext(), "Muscle Gain Diet", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
+        i3_balanced.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Fragment balancedFrag=new BalancedFragment();
+                FragmentManager fragmentManager=getParentFragmentManager();
+                FragmentTransaction transaction=fragmentManager.beginTransaction();
+                transaction.replace(R.id.framelayout,balancedFrag);
+                transaction.addToBackStack(null);
+                transaction.commit();
+
+                Toast.makeText(getContext(), "Balanced Diet", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
+        i4_highprotein.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Fragment highproFrag=new HighProteinFragment();
+                FragmentManager fragmentManager=getParentFragmentManager();
+                FragmentTransaction transaction=fragmentManager.beginTransaction();
+                transaction.replace(R.id.framelayout,highproFrag);
+                transaction.addToBackStack(null);
+                transaction.commit();
+
+                Toast.makeText(getContext(), "High Protein Diet", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
+
+        return v;
     }
 }
