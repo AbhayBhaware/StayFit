96e52ee1f6e061781884384afc60bbee9db6c2f7
AbhayBhaware <abhaybhaware20@gmail.com>
2025-02-22 15:47:07 +0530

    Muscle gain video add

--- app/src/main/java/com/example/stayfit/MuscleGainFragment.java
+++ app/src/main/java/com/example/stayfit/MuscleGainFragment.java
@@ -1,5 +1,6 @@
 package com.example.stayfit;
 
+import android.net.Uri;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,58 +8,43 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.VideoView;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link MuscleGainFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class MuscleGainFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public MuscleGainFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment MuscleGainFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static MuscleGainFragment newInstance(String param1, String param2) {
-        MuscleGainFragment fragment = new MuscleGainFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
+    Button pauseBTN;
+    VideoView v1Vid;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_muscle_gain, container, false);
+        View v= inflater.inflate(R.layout.fragment_muscle_gain, container, false);
+
+        pauseBTN=v.findViewById(R.id.playBTN2);
+        v1Vid=v.findViewById(R.id.musclegainVID);
+
+        Uri vidUri=Uri.parse("android.resource://"+getActivity().getPackageName()+"/"+R.raw.musclegainvid);
+        v1Vid.setVideoURI(vidUri);
+
+        pauseBTN.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (v1Vid.isPlaying())
+                {
+                    v1Vid.pause();
+                    pauseBTN.setText("PLAY");
+                }
+                else {
+                    v1Vid.start();
+                    pauseBTN.setText("PAUSE");
+                }
+            }
+        });
+
+
+        return v;
     }
 }
